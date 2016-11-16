74b01f7f807249c5bec517ab4ab0bb7372a21d99
Veronica Sonzini <mavesonzini@gmail.com>
2016-11-16 16:04:14 +0000

    Creating the station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,16 @@
+require 'station.rb'
+
+describe Station do
+
+  describe "#initialization" do
+    subject(:station) { described_class.new("Old Street", 1) }
+
+    it 'has a name' do
+      expect(subject.name).to eq "Old Street"
+    end
+
+    it 'has a zone' do
+      expect(subject.zone).to eq 1
+    end
+  end
+end
